556c0e912f90143137e798908de79930125852a3
Brianna Sparks <briannasparks@Briannas-MacBook-Air.local>
2025-12-26 23:00:54 -0800

    starting longestPalindromSubstring

---
+++ Neetcode150/LongestPalindromeSubstring/test.py
@@ -0,0 +1,30 @@
+from solution import Solution
+
+def run_tests():
+    obj = Solution()
+
+    test_num = 1
+    s = "babad"
+    expected = "bab"
+
+    actual = obj.longestPalindrome(s)
+
+    assert actual == expected, (
+        f"Test {test_num} failed, longestPalindrome({s}) expected {expected} but returned {actual}."
+    )
+
+    test_num += 1
+    s = "cbbd"
+    expected = "bb"
+
+    actual = obj.longestPalindrome(s)
+
+    assert actual == expected, (
+        f"Test {test_num} failed, longestPalindrome({s}) expected {expected} but returned {actual}."
+    )
+
+run_tests()
+
+
+    
+
