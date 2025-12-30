094da88678a7f39c30d76a344a91b1ee059ac142
Brianna Sparks <briannasparks@Briannas-MacBook-Air.local>
2025-12-30 01:07:14 -0800

    first cs compilation here

---
+++ Neetcode150/TwoSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = Solution.TwoSum(nums, target);
+
+        if (nums[result[0]] + nums[result[1]] != target)
+            throw new Exception("Test failed");
+
+        Console.WriteLine("Test passed!");
+    }
+}
---
+++ Neetcode150/TwoSum/TwoSum.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public static int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+
+            // store value -> index
+            map[nums[i]] = i;
+        }
+
+        return new int[0]; // or throw exception if guaranteed solution exists
+    }
+}
+
